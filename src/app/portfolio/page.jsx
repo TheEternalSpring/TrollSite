12f4833f2c21ee8cf2ef15ff3142d903fa452c4e
TheEternalSpring <sababuch@gmail.com>
2023-12-23 21:29:04 +0400

    changed remaining pages, footer and added WIP content 4 contacts and portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
+import Link from 'next/link'
 
 function page() {
   return (
-    <div>what we made</div>
+    <div>
+      <h2 className={styles.maintitle}>Our Works</h2>
+
+      <div className={styles.space}></div>
+
+      <div className={styles.collage}>
+
+        <div className={styles.foto}>
+          <Link href={"/"}>
+          <h3 className={styles.title}>People</h3>
+          <Image className={styles.foto1} src={"/trollman.gif"} width={300} height={500}/>
+          </Link>
+        </div>
+
+        <div className={styles.foto}>
+          <Link href={"/"}>
+          <h3 className={styles.title}>Events</h3>
+          <Image className={styles.foto1} src={"/gagnet.gif"} width={300} height={500}/>
+          </Link>
+        </div>
+
+        <div className={styles.foto}>
+          <Link href={"/"}>
+          <h3 className={styles.title}>Photos</h3>
+          <Image className={styles.foto1} width={300} height={500} src={"/makeup.jpg"}/>
+          </Link>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/app/portfolio/photos/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./page.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
+
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
